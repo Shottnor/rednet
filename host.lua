60dabb8f1792814c83ccfc8fc58762cacf553bdd
shottnor <cmdrloknor@gmail.com>
2023-03-14 12:43:19 +0100

    added button test

--- host.lua
+++ host.lua
@@ -1,19 +1,19 @@
 local networkname = "tanktest"
- 
+
 rednet.open("right")
 rednet.host(networkname, "host")
- 
+
 local ids = {rednet.lookup(networkname)}
 local theStorageArray = {}
 for i = 1, 40 do
     theStorageArray[i] = {}
 end
- 
+
 while true do
-    
- 
+
+
     for i=0,#ids do
- 
+
         if ids[i] ~= rednet.lookup(networkname, "host") and ids[i] ~= nil  then
             repeat
                 id, message = rednet.receive()
@@ -23,15 +23,14 @@ while true do
             print(message.name)
             term.setCursorPos(30,i)
             print(message.amount)
-            
-           
-        end
- 
- 
-    end
-    
 
- 
+         end
+
+
+    end
+
+
+
 end
 
 
