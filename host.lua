f5d94600f317ba650f25d3f416db99003944a239
shottnor <cmdrloknor@gmail.com>
2023-03-14 13:50:51 +0100

    return to a working state

--- host.lua
+++ host.lua
@@ -1,23 +1,18 @@
 local networkname = "tanktest"
 
+
+
 rednet.open("right")
 rednet.host(networkname, "host")
 
-local ids = {rednet.lookup(networkname)}
-local theStorageArray = {}
-for i = 1, 40 do
-    theStorageArray[i] = {}
-end
-
-while true do
-
+function UpdateUi(ids)
 
     for i=0,#ids do
 
         if ids[i] ~= rednet.lookup(networkname, "host") and ids[i] ~= nil  then
             repeat
                 id, message = rednet.receive()
-                print("awaiting message from", ids[i])
+                
             until id == ids[i]
             term.setCursorPos(1,i)
             print(message.name)
@@ -28,6 +23,18 @@ while true do
 
 
     end
+
+end
+
+
+local ids = {rednet.lookup(networkname)}
+
+
+
+
+while true do
+
+UpdateUi(ids)
 
 
 
